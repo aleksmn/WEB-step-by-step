2f148eb5a9043fb799602ad0b61182a5e26d5348
aleksmn <al-mn@yandex.ru>
2021-11-04 11:35:31 +0300

    js example with button

--- JS-DOM-model/index.js
+++ JS-DOM-model/index.js
@@ -1,2 +1,16 @@
-document.querySelector("h1").innerHTML="Good Bye!"
+function myFunction() {
 
+    // Вставить HTML внутрь элемента
+    document.querySelector("h1").innerHTML="<em>Good Bye!</em>"
+
+    // Добавить класс
+    document.querySelector("h1").classList.toggle("huge")
+
+    // Сменить атрибут тега
+    document.querySelector("a").setAttribute('href', "https://yandex.ru");
+
+}
+
+function time() {
+    document.getElementById('time').innerHTML=Date()
+}
