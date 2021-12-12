61668bb8ca9c8c6394862d33ec7368dbfbd1adad
aleksmn <al-mn@yandex.ru>
2021-12-12 19:43:25 +0300

    add lesson 25

---
+++ WEB-lessons/Урок-25/script.js
@@ -0,0 +1,26 @@
+function changeStyle(){
+
+    let button = document.getElementById('btn-1');
+
+    
+    // проверка цвета фона
+    
+    if (window.getComputedStyle(button).backgroundColor == "rgb(207, 17, 17)")
+    {
+        button.style.backgroundColor = "green";
+        console.log("change to green")
+    }
+    else 
+        button.style.backgroundColor = "rgb(207, 17, 17)";
+        console.log("change to red")
+
+}
+
+
+function toggleClass(){
+    let button = document.getElementById('btn-2');
+
+    button.classList.toggle("bg-green");
+
+    console.log("toggle class")
+}
