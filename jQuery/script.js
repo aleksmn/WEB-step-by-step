6fc7cd4cb2625ce168f9728d5e866ff3558abe06
aleksmn <al-mn@yandex.ru>
2021-12-15 13:07:41 +0300

    jquery examples

--- jQuery/script.js
+++ jQuery/script.js
@@ -1,4 +1,27 @@
 $("h1").addClass("big-title");
-$("h1").removeClass("big-title");
 
-$("button").addClass("red-btn");
+$("button").addClass("green-btn margin-10");
+
+$(".btn-1").click(function () { 
+    $("h1").removeClass("big-title");
+    $("h1").text("Пока!")
+});
+
+$(".btn-2").click(function () { 
+    $("h1").toggleClass("big-title");
+    
+});
+
+$(".btn-3").click(function () { 
+    $("button").addClass("red-btn");
+    $("button").text("Не нажимать!")
+});
+
+
+$("button").click(function () {
+    if ($("button").hasClass("red-btn")) {
+        $(this).slideUp();
+    } 
+});
+
+
