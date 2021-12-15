8ee0b73218479904136d19542c24359085a21fc2
aleksmn <al-mn@yandex.ru>
2021-12-15 16:00:31 +0300

    add lesson 28

---
+++ WEB-lessons/Урок-28/rating/script.js
@@ -0,0 +1,13 @@
+function star(k) {
+
+    var ids = ["star-1", "star-2", "star-3", "star-4", "star-5"];
+
+    for (let i=0; ids.length; i++) {
+
+        b = ids[i];
+        a = document.getElementById(b);
+        a.src = "images/star_1.png";
+    }
+
+
+}
