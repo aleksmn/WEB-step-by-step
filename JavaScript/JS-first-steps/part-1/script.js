92d864aa691ce90d90217d9d06253b31dca00422
aleksmn <al-mn@yandex.ru>
2022-01-05 17:59:34 +0300

    start js first steps part 1

---
+++ JavaScript/JS-first-steps/part-1/script.js
@@ -0,0 +1,57 @@
+// console.log("Привет, Мир!")
+
+/*
+let js = 'fun';
+if (js === 'fun') alert('Javascript is FUN!');
+
+console.log(40 + 8 + 5 + 12);
+
+let firstName = "Михаил";
+console.log(firstName);
+
+let lastName = "Александров"
+
+console.log(firstName + ' ' + lastName);
+
+// let 32 = 32;                 // error
+// let function = "hello";      // error
+
+
+
+//////// Data Types //////////
+
+//// 1. Number: 
+let age = 21;
+//// 2. String:
+let firstName = "Михаил";
+//// 3. Boolean
+let human = true;
+//// 4. Undefined
+let lastName;
+
+
+console.log(typeof lastName);
+console.log(typeof firstName);
+console.log(typeof human);
+
+
+
+////// Область видимости ///////
+
+{
+    let a = "hello";
+    var b = "привет!";
+}
+
+console.log(b);
+let a = "новое значение";
+
+let x = 7;
+let x = 'hi';   //// SyntaxError: Identifier 'x' has already been declared
+
+var y = 8;
+var y = "hello";
+
+console.log(y);
+
+*/
