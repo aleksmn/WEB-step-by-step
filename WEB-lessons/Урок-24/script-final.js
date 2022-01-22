381c3e1cdc75dd84aab4d20937b42de844b833ce
aleksmn <al-mn@yandex.ru>
2022-01-22 11:56:26 +0300

    add final to lesson 24

---
+++ WEB-lessons/Урок-24/script-final.js
@@ -0,0 +1,37 @@
+// console.log("Привет!");
+
+// Калькулятор
+
+function calc() {
+    let a = parseFloat(prompt("Введите a"));
+    let b = parseFloat(prompt("Введите b"));
+
+    alert("a + b = " + (a+b) 
+      + "\na - b= " + (a-b) 
+      + "\na * b =" + (a*b));
+
+    console.log(typeof a, typeof b);
+}
+
+// Работа с текстом
+
+function textCalc() {
+
+    let text = prompt('Введите текст: ');
+    console.log(text);
+
+    console.log("Длина текста: " + text.length);
+    console.log("Первая буква: " + text[0])
+    console.log("Последняя буква: " + (text[text.length - 1]))
+
+    document.getElementById("text").innerText = "Текст: " + text + "\nДлина текста: " + text.length 
+                                                + "\nПервая буква: " + text[0]
+                                                + "\nПоследняя буква: " + (text[text.length - 1]);
+
+}
+
+// Вызываем функции:
+
+// calc()
+
+// textCalc()
