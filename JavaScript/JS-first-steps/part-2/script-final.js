25d8ac8883d93bd45b923bb62cabe8b8dd45836d
aleksmn <al-mn@yandex.ru>
2022-02-01 12:55:31 +0300

    add challenge 2.2 to JS part 2

--- JavaScript/JS-first-steps/part-2/script-final.js
+++ JavaScript/JS-first-steps/part-2/script-final.js
@@ -220,3 +220,58 @@ if (users.includes("Артем")) {
 
 
 */
+
+
+
+//////  Задание 2.2 /////////
+
+
+/*
+Напишите функцию calcTip() для вычисления чаевых:
+чаевые 15%, если счет 50 - 300
+чаевые 20%, если счет больше.
+
+Используйте массив, для определения чаевых для 
+нескольких счетов:
+bills - массив счетов
+tips - массив чаевых
+totals - итоговы значения (счет + чаевые)
+
+Тестовые значения (счета): 
+125, 455, 44
+
+*/
+
+/*
+function calcTip(bill) {
+    if (bill >= 50 && bill <= 300) {
+        return bill * 0.15;
+    } else if (bill > 300) {
+        return bill * 0.2;
+    }
+
+    return 0;
+}
+
+const bills = [125, 455, 44];
+// const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])]
+// console.log(bills, tips);
+
+
+///// Используем цикл for.
+const tips = [];
+const totals = [];
+for (let i = 0; i < bills.length; i++) {
+    // console.log(i);
+    tips.push(calcTip(bills[i]));
+    totals.push(bills[i] + tips[i]);
+}
+
+console.log(bills, tips, totals);
+
+
+*/
+
+
+
+
