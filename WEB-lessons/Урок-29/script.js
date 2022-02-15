f9bcfd02da7da6d9ccbe8474308a60fdd2e7a38f
aleksmn <al-mn@yandex.ru>
2022-02-15 17:57:02 +0300

    fix rating

--- WEB-lessons/Урок-29/script.js
+++ WEB-lessons/Урок-29/script.js
@@ -1,13 +1,8 @@
 function star(k) {
     // k - номер звезды на которую навел пользователь
-
     k = k - 1;
-
     let ids = ["star-1", "star-2", "star-3", "star-4", "star-5"];
-
     // console.log(ids[k]);
-
-
     let star = document.getElementById(ids[k]);
 
 
@@ -16,9 +11,9 @@ function star(k) {
     // // если звезда пустая
     if (star.src.includes("star_2.png")) {
 
-        // то меняем ее на заполненную
+        // то меняем ее и все предыдущие на заполненные
         for (let i = 0; i <= k; i++) {
-            id = ids[i]
+            let id = ids[i];
             star = document.getElementById(id);
             star.src = "star_1.png";
         }
