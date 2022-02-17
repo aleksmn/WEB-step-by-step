d9ee5e9512c55dcd23a7cc7dd0aef2b9ba0e2d5b
aleksmn <al-mn@yandex.ru>
2022-02-17 15:56:11 +0300

    fixes

--- WEB-lessons/Урок-25/script.js
+++ WEB-lessons/Урок-25/script.js
@@ -1,27 +1,37 @@
-function changeStyle(){
+function changeStyle() {
+
     //Выбираем нужный элемент на страничке:
     let myButton = document.getElementById('btn-1');
+
+    console.log(myButton);
+
+
     // текущий цвет:
     let currColor = window.getComputedStyle(myButton).backgroundColor;
-    const firstColor = "green";
-    const secondColor = "rgb(207, 17, 17)";
-    
+
+    console.log(currColor);
+    // первый (зеленый) цвет:
+    const firstColor = "rgb(47, 129, 47)";
+    //второй (красный) цвет:
+    const secondColor = "rgb(207, 23, 23)";
+
     // проверка цвета фона кнопки
-    if (currColor === secondColor)
-    {
+    if (currColor === secondColor) {
+
         myButton.style.backgroundColor = firstColor;
-        console.log("change to green");
+        // console.log("change to green");
     }
+
     else {
         myButton.style.backgroundColor = secondColor;
-        console.log("change to red");
+        // console.log("change to red");
     }
 
 }
 
 
 
-function toggleClass(){
+function toggleClass() {
     let myButton = document.getElementById('btn-2');
 
     myButton.classList.toggle("bg-green");
