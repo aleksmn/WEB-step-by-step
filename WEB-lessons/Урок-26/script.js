47e2b4a28fd01b2420e7388bd4044aeb8cb6b6a4
aleksmn <al-mn@yandex.ru>
2022-02-18 17:54:02 +0300

    fix loop lesson

---
+++ WEB-lessons/Урок-26/script.js
@@ -0,0 +1,37 @@
+let text = "";
+
+for (let i = 0; i < 5; i++) {
+
+
+    text += "<h3>Номер " + i + "</h3>";
+
+    // text = text + "<h3>Номер " + i + "</h3>";
+
+
+}
+
+document.getElementById("demo").innerHTML= text;
+
+
+
+// document.getElementById("demo").innerHTML = text;
+
+
+// Массивы
+
+
+const users = ["Кирилл", "Дмитрий", "Иван", "Артем"];
+
+console.log(users[0]);
+console.log(users.length);
+
+// let text = '';
+
+// for (let i = 0; i < users.length; i++) {
+
+//     text += users[i] + "<br>";
+
+
+// }
+// document.getElementById("demo").innerHTML = text;
+
