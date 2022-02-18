7d013cd393144c5b2e8457f3f48ceed6e584e562
aleksmn <al-mn@yandex.ru>
2022-02-18 17:58:15 +0300

    add to l26

--- WEB-lessons/Урок-26/script.js
+++ WEB-lessons/Урок-26/script.js
@@ -35,3 +35,60 @@ console.log(users.length);
 // }
 // document.getElementById("demo").innerHTML = text;
 
+
+
+
+
+////// Методы массива ///////
+
+
+/*
+// Добавление элементов
+users.push('Алексей');
+users.unshift('Михаил');
+
+console.log(users);
+
+// Удаление элементов
+console.log(users.pop());
+console.log(users.shift());
+
+console.log(users);
+
+console.log(users.indexOf("Дмитрий"));
+console.log(users.indexOf("Юлия"));
+
+console.log(users.includes("Дмитрий"));
+console.log(users.includes("Юлия"));
+
+if (users.includes("Артем")) {
+    console.log("Есть пользователь с именем Артем");
+}
+
+
+*/
+
+
+/*
+
+// Функция удаления элемента
+
+const users = [ "Артем", "Дмитрий","Кирилл", "Иван"];
+
+
+function delItem(myArr, item) {
+    const itemToDel = item;
+    const indexToDel = myArr.indexOf(itemToDel);
+
+    if (indexToDel > 0) {
+        myArr.splice(indexToDel, 1);
+    } else {
+        console.log("Элемент не найден!");
+    }
+}
+
+delItem(users, "Дмитрий");
+
+console.log(users);
+
+*/
