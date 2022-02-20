8a4ff3e98291d72bef2848b3a8203d97b17622cb
aleksmn <al-mn@yandex.ru>
2022-02-20 18:52:10 +0300

    weather app init

---
+++ JavaScript/WeatherApp/script.js
@@ -0,0 +1,19 @@
+// API Link:
+// https://openweathermap.org/
+
+
+// Get weather from API
+// api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+
+
+let weather = {
+    "apiKey": "66b5d8e6815556797faf3e3ca7778709"
+}
+
+
+
+
+
+
+
+
