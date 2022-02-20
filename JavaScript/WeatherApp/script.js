b5bedeb0b49e0b48d4bebce7f5b5484056d7a8cc
aleksmn <al-mn@yandex.ru>
2022-02-20 21:07:33 +0300

    final weather app

--- JavaScript/WeatherApp/script.js
+++ JavaScript/WeatherApp/script.js
@@ -44,6 +44,38 @@ const weather_api_key = config.WEATHER_API_KEY;
 /////// STEP 2 ///////
 
 
+// let weather = {
+//     "apiKey": weather_api_key,
+//     fetchWeather: function (city) {
+//         fetch(
+//             "https://api.openweathermap.org/data/2.5/weather?q="
+//             + city
+//             + "&units=metric&appid="
+//             + this.apiKey
+//         )
+//             .then((responce) => responce.json())
+//             .then((data) => this.displayWeather(data));
+//     },
+
+//     displayWeather: function (data) {
+//         const { name } = data;
+//         const { icon, description } = data.weather[0];
+//         const { temp, humidity } = data.main;
+//         const { speed } = data.wind;
+//         console.log(name, icon, description, temp, humidity, speed);
+//     }
+// };
+
+// //// проверка:
+// weather.fetchWeather('london');
+
+
+
+
+/////// STEP 3 ///////
+
+
+
 let weather = {
     "apiKey": weather_api_key,
     fetchWeather: function (city) {
@@ -62,16 +94,42 @@ let weather = {
         const { icon, description } = data.weather[0];
         const { temp, humidity } = data.main;
         const { speed } = data.wind;
-        console.log(name, icon, description, temp, humidity, speed);
-    }
+        // console.log(name, icon, description, temp, humidity, speed);
+        document.querySelector(".city").innerHTML = "Weather in " + name;
+        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temp").innerText = temp + "°C";
+        document.querySelector(".humidity").innerText =
+            "Humidity: " + humidity + "%";
+        document.querySelector(".wind").innerText =
+            "Wind speed: " + speed + " km/h";
+        document.querySelector(".weather").classList.remove("loading");
+        document.body.style.backgroundImage =
+            "url('https://source.unsplash.com/1600x900/?" + name + "')";
+    },
+
+    search: function () {
+        this.fetchWeather(document.querySelector(".search-bar").value);
+    },
 };
 
-//// проверка:
-weather.fetchWeather('london');
+////// проверка:
+// weather.fetchWeather('london');
 
 
+document.querySelector(".search button").addEventListener("click", function () {
+    weather.search();
+});
 
 
-/////// STEP 3 ///////
+document
+    .querySelector(".search-bar")
+    .addEventListener("keyup", function (event) {
+        if (event.key == "Enter") {
+            weather.search();
+        }
+    });
 
+
+weather.fetchWeather("Moscow");
 
