c7fbd39be2eaccb15e552222ff1fc94d43cb1e2c
aleksmn <al-mn@yandex.ru>
2022-04-01 12:55:52 +0300

    add siteRating variable

--- WEB-lessons/lesson-29/betterRating/script.js
+++ WEB-lessons/lesson-29/betterRating/script.js
@@ -1,17 +1,17 @@
 let stars = document.querySelectorAll(".rating img");
+let siteRating = 0;
 
-for (let i = 0; i< stars.length; i++) {
+for (let i = 0; i < stars.length; i++) {
     stars[i].addEventListener("mouseover", () => {
         stars.forEach(star => {
             star.src = "images/star_2.png";
-            // star.style.opacity = 0.5
         });
 
-        for(let k = 0; k <= i; k++) {
+        for (let k = 0; k <= i; k++) {
             stars[k].src = "images/star_1.png";
-            // stars[k].style.opacity = 1;
+            siteRating = k + 1;
+            console.log("Рейтинг сайта:", siteRating);
         }
-        
+
     });
 };
-
