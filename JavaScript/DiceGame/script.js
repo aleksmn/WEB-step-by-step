e5be73f9c540347f3969b1132f9afd9b134b90a7
aleksmn <al-mn@yandex.ru>
2022-04-01 15:52:26 +0300

    fix script

--- JavaScript/DiceGame/script.js
+++ JavaScript/DiceGame/script.js
@@ -1,9 +1,10 @@
-function play() {
+const playBtn = document.getElementById("play-btn");
 
+
+playBtn.addEventListener('click', function () {
     // Создаем случайные числа от 1 до 6 для каждого игрока
     let randomNumber1 = Math.floor(Math.random() * 6 + 1);
     let randomNumber2 = Math.floor(Math.random() * 6 + 1);
-    console.log(randomNumber1, randomNumber2);
 
     // Подбираем картинку под полученные числа
     let src1 = "images/dice" + randomNumber1 + ".png";
@@ -12,7 +13,11 @@ function play() {
     let img1 = document.querySelector(".img1").setAttribute("src", src1);
     let img2 = document.querySelector(".img2").setAttribute("src", src2);
 
+
+
     // Выводим результат на экран
+    // https://emojipedia.org/triangular-flag/
+
     let result;
 
     if (randomNumber1 > randomNumber2) {
@@ -28,4 +33,6 @@ function play() {
     }
 
     document.querySelector("h1").innerText = result;
-}
+
+    
+});
