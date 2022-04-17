dd1d11b5544c9d38bc39dffc560adcec0b42d40c
aleksmn <al-mn@yandex.ru>
2022-04-17 17:53:10 +0300

    init

--- JavaScript/DrumKit/index.js
+++ JavaScript/DrumKit/index.js
@@ -1,13 +1,63 @@
-// Add event listeners to all buttons
+const drumButtons = document.querySelectorAll(".drum")
 
-var drumButtons = document.querySelectorAll(".drum")
+// console.log(drumButtons);
 
-for (var i = 0; i < drumButtons.length; i++) {
+// for (let i = 0; i < drumButtons.length; i++) {
     
-    drumButtons[i].addEventListener("click", function () {
-        alert("Clicked!!")
+//     drumButtons[i].addEventListener("click", function () {
+//         alert("Clicked!!")
+//     });
+// }
+
+
+// Используем FOR OF
+
+for (btn of drumButtons) {
+
+    btn.addEventListener("click", function () {
+        // this - елемент, на который кликнули
+        // console.log(this);
+
+        // console.log(this.classList.value.includes('w'));
+
+        // if (this.classList.value.includes('w')) {
+        //     new Audio('sounds/tom-1.mp3').play()
+        //     return
+        // }
+        // if (this.classList.value.includes('a')) {
+        //     new Audio('sounds/tom-2.mp3').play()
+        //     return
+        // }
+        
+        this.style.color = 'white';
+
+        switch (this.classList[0]) {
+            case 'w':
+                new Audio('sounds/tom-1.mp3').play();
+                break;
+
+            case 'a':
+                new Audio('sounds/tom-2.mp3').play();
+                break;
+
+            case 's':
+                new Audio('sounds/tom-3.mp3').play();
+                break;
+
+            case 'd':
+                new Audio('sounds/tom-4.mp3').play();
+                break;
+        
+            default:
+                break;
+        }
+
+        // let tom1 = new Audio('sounds/tom-1.mp3');
+        // tom1.play()
+
     });
 }
+
 
 
 
