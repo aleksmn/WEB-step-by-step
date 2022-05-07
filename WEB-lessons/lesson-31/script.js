e057ce1b95bd297d9f197b19ead070bcc9187b46
aleksmn <al-mn@yandex.ru>
2022-05-07 18:19:00 +0300

    fix string

--- WEB-lessons/lesson-31/script.js
+++ WEB-lessons/lesson-31/script.js
@@ -1,33 +1,31 @@
 const form = document.getElementById("form");
 
 form.addEventListener("submit", function (event) {
-
-    // console.log(event);
-    event.preventDefault();
-    // console.log("Форма отправлена!");
-    validForm();
+  // console.log(event);
+  event.preventDefault();
+  // console.log("Форма отправлена!");
+  validForm();
 });
 
-
 function validForm() {
+  const age = document.getElementById("age").value;
+  const country = document.getElementById("country").value;
+  const email = document.getElementById("email").value;
 
-    const age = document.getElementById("age").value;
-    const country = document.getElementById("country").value;
-    const email = document.getElementById("email").value;
-
-    // // Получаем значение из инпута radio 
-    const passport =  document.querySelector('input[name="passport"]:checked').value;
-
-
-    const text = "Отправлены следующие данные:\n"
-            + "Ваш возраст: " + age
-            + "\nСтрана проживания: " + country
-            + "\nЭлектронная почта: " + email
-            + "\nЗагранпаспорт: " + passport;
+  // // Получаем значение из инпута radio
+  const passport = document.querySelector(
+    'input[name="passport"]:checked'
+  ).value;
 
 
-    document.getElementById("data").innerText = text;
+  const text = `
+        <h2>Отправлены следующие данные:</h2>
 
+        Ваш возраст: ${age};
+        Страна проживания: ${country};
+        Электронная почта: ${email};
+        Загранпаспорт: ${passport}.
+        `;
+
+  document.getElementById("data").innerHTML = text;
 }
-
-
