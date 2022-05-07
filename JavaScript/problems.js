4c429a0938778dcd75b1556127fe0b0ed4ef3cb6
aleksmn <al-mn@yandex.ru>
2022-05-07 09:53:38 +0300

    add palindrom

--- JavaScript/problems.js
+++ JavaScript/problems.js
@@ -108,3 +108,27 @@
 
 
 // console.log(myString);
+
+
+
+// Написать функцию, которая проверяет, является ли строка палиндромом, например:
+// palindrome('доход') --> true
+// palindrome('abcd') --> false
+
+
+
+
+
+// function palindrome(str) {
+//   let re = /[^A-Za-z0-9]/g;
+//   str = str.toLowerCase().replace(re, "");
+
+//   let len = str.length;
+//   for (let i = 0; i < len / 2; i++) {
+//     if (str[i] !== str[len - 1 - i]) {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
+// palindrome("A man, a plan, a canal. Panama");
