e0d0a9d283850736f751c417c4770861e35f4817
aleksmn <al-mn@yandex.ru>
2022-05-07 12:08:16 +0300

    init

---
+++ JavaScript/JS-first-steps/part-2/objects.js
@@ -0,0 +1,29 @@
+///// Введение в объекты (Objects)
+
+
+
+// const user1 = {
+//   name: "Dima",
+//   age: 18,
+// };
+
+// const user2 = {
+//   name: "Vlad",
+//   age: 21,
+// };
+
+// console.log(`Hello, my name is ${user1.name}!`);
+// console.log(`Hello, my name is ${user2.name}!`);
+
+
+
+
+// class User {
+//   constructor(name, age) {
+//     this.name = name;
+//     this.age = age;
+//   }
+//   speak() {
+//     console.log(`Hello, my name is ${this.name}, i am ${this.age}.`);
+//   }
+// }
