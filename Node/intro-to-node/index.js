7b4704b4817a6b77b91797ac737aa1b1c5b96e6f
aleksmn <al-mn@yandex.ru>
2022-05-09 10:54:14 +0300

    testing node

---
+++ Node/intro-to-node/index.js
@@ -0,0 +1,11 @@
+// const fs = require("fs");
+
+// fs.copyFileSync("file1.txt", "file2.txt");
+
+const superheroes = require("superheroes");
+
+
+let mySuperhero = superheroes.random();
+
+console.log(mySuperhero);
+
