b89ca38db2604bdd8b4f539d26a8bc1d94081b68
aleksmn <al-mn@yandex.ru>
2022-05-11 18:31:09 +0300

    init

---
+++ Node/my-express-server/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello World!</h1>");
+  // console.log(req);
+});
+
+
+app.get("/contacts", (req, res) => {
+  res.send("<h1>Contact Us! Hello!</h1>");
+});
+
+app.get("/about", (req, res) => {
+  res.send("<h1>About Page</h1>");
+});
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
