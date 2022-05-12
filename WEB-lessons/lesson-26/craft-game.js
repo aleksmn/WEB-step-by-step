7fe876a0c7a3276d1486e051c1351e24afffb36f
aleksmn <al-mn@yandex.ru>
2022-05-12 10:01:05 +0300

    add button

--- WEB-lessons/lesson-26/craft-game.js
+++ WEB-lessons/lesson-26/craft-game.js
@@ -1,18 +1,27 @@
 const myDiv = document.getElementById("demo");
 
-let winCount = 0;
+myDiv.innerHTML += '<button id="craft" type="button">Начать крафт</button>';
 
-for (let i = 1; i <= 10; i++) {
+const craftBtn = document.getElementById("craft");
+
+
+craftBtn.onclick = function() {
+
+  let winCount = 0;
+
+  for (let i = 1; i <= 10; i++) {
+    setTimeout(function () {
+      if (Math.random() > 0.5) {
+        myDiv.innerHTML += `<p>Шаг ${i}: Удача!</p>`;
+        winCount += 1;
+      } else {
+        myDiv.innerHTML += `<p>Шаг ${i}: Неудача!</p>`;
+      }
+    }, i * 1000);
+  }
+
   setTimeout(function () {
-    if (Math.random() > 0.5) {
-      myDiv.innerHTML += `<p>Шаг ${i}: Удача!</p>`;
-      winCount += 1;
-    } else {
-      myDiv.innerHTML += `<p>Шаг ${i}: Неудача!</p>`;
-    }
-  }, i * 1000);
-}
+    myDiv.innerHTML += `<p><strong>Удачных попыток: ${winCount}</strong></p>`;
+  }, 11000);
 
-setTimeout(function () {
-  myDiv.innerHTML += `<p>У тебя удачных попыток: ${winCount}</p>`;
-}, 11000);
+}
