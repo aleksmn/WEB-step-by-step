94be8af205af8c794329ab0f482896a7871b9747
aleksmn <al-mn@yandex.ru>
2022-05-16 11:57:48 +0300

    add post to server

--- Node/date-calculator/datecalc.js
+++ Node/date-calculator/datecalc.js
@@ -3,8 +3,14 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Date Calculator");
+  // res.send("Date Calculator");
+  res.sendFile(__dirname+ "/index.html")
+  // console.log(__dirname);
 });
+
+app.post("/", function(req, res){
+  res.send("Thanks for posting that!")
+})
 
 app.listen(port, () => {
   console.log(`Date calculator app listening on port ${port}`);
