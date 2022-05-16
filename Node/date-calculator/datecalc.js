0b612128100cb308342d150d8e11cb49f5f55f2d
aleksmn <al-mn@yandex.ru>
2022-05-16 21:49:04 +0300

    finish calc

--- Node/date-calculator/datecalc.js
+++ Node/date-calculator/datecalc.js
@@ -1,16 +1,27 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.urlencoded({extended: true}))
+
 app.get("/", (req, res) => {
   // res.send("Date Calculator");
-  res.sendFile(__dirname+ "/index.html")
+  res.sendFile(__dirname+ "/index.html");
   // console.log(__dirname);
 });
 
 app.post("/", function(req, res){
-  res.send("Thanks for posting that!")
-})
+  console.log(req.body);
+  let date1 = new Date(req.body.date1); 
+  let date2 = new Date(req.body.date2);
+
+  let timeDiff = date2.getTime() - date1.getTime();
+  let daysDiff = timeDiff / (1000 * 3600 * 24); 
+
+  res.send("Количество дней между датами: " + daysDiff);
+});
 
 app.listen(port, () => {
   console.log(`Date calculator app listening on port ${port}`);
