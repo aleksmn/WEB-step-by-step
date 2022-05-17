6d8733d8de3cfc45afb59dd1b660384d34dd3a81
aleksmn <al-mn@yandex.ru>
2022-05-17 15:48:27 +0300

    add static files

--- Node/date-calculator/datecalc.js
+++ Node/date-calculator/datecalc.js
@@ -1,26 +1,32 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 
 const app = express();
 const port = 3000;
 
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(express.static(__dirname + "/public"));
+
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   // res.send("Date Calculator");
-  res.sendFile(__dirname+ "/index.html");
+  res.sendFile(__dirname + "/index.html");
   // console.log(__dirname);
 });
 
-app.post("/", function(req, res){
+app.get("/about", (req, res) => {
+  res.sendFile(__dirname + "/about.html");
+});
+
+app.post("/", (req, res) => {
   console.log(req.body);
-  let date1 = new Date(req.body.date1); 
+  let date1 = new Date(req.body.date1);
   let date2 = new Date(req.body.date2);
 
   let timeDiff = date2.getTime() - date1.getTime();
-  let daysDiff = timeDiff / (1000 * 3600 * 24); 
+  let daysDiff = timeDiff / (1000 * 3600 * 24);
 
   res.send("Количество дней между датами: " + daysDiff);
+
 });
 
 app.listen(port, () => {
