cd4a10db105f9d40e8795c803e9a35501d0ba548
aleksmn <al-mn@yandex.ru>
2022-06-08 17:02:26 +0300

    add images path

--- Node/spa-params/server.js
+++ Node/spa-params/server.js
@@ -2,14 +2,22 @@ const express = require("express");
 const path = require("path");
 const port = 5555;
 
-
 const app = express();
 
-app.use("/static", express.static(path.resolve(__dirname + "/frontend/static")));
+app.use(
+  "/static",
+  express.static(path.resolve(__dirname + "/frontend/static"))
+);
+
+app.use(
+  "/images",
+  express.static(path.resolve(__dirname + "/frontend/static/images"))
+);
 
 app.get("/*", (req, res) => {
   res.sendFile(path.resolve(__dirname + "/frontend/index.html"));
 });
 
-
-app.listen(port, () => console.log(`Сервер работает... Порт: ${port}`))
+app.listen(port, () =>
+  console.log(`Сервер работает... Порт: http://localhost:${port}`)
+);
