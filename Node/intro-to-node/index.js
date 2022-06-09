f5710b2fd5fcf1f28a22dc63262939ef98bf64fd
aleksmn <al-mn@yandex.ru>
2022-06-09 19:02:53 +0300

    add comments

--- Node/intro-to-node/index.js
+++ Node/intro-to-node/index.js
@@ -2,9 +2,14 @@
 
 // fs.copyFileSync("file1.txt", "file2.txt");
 
-
 // // Docs:
 // // https://nodejs.org/api/fs.html
+
+// // Initialize NPM with 'npm init' command
+
+// // Install 'npm i superheroes'
+
+// // https://www.npmjs.com/package/superheroes
 
 const superheroes = require("superheroes");
 
