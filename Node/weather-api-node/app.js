6d82dd45d8da2238e8d6a7e1a2089e8470fa8767
aleksmn <al-mn@yandex.ru>
2022-07-05 19:39:47 +0300

    add frontend to weather app

--- Node/weather-api-node/app.js
+++ Node/weather-api-node/app.js
@@ -1,23 +1,46 @@
+const { response } = require("express");
 const express = require("express");
 const https = require("https");
 const port = 3000;
 const app = express();
 
+require("dotenv").config();
 
+
+app.use(express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
-  const url = "https://api.openweathermap.org/data/2.5/weather?appid=7156236ecba085335a43e6b6cf40deeb&q=moscow&units=metric&lang=ru"
-  
-  https.get(url, (res) => {
-    console.log(res.statusCode, res.statusMessage); /// -> 200 OK
 
+  res.sendFile(__dirname + "/index.html");
+})
 
+app.get("/weather", (req, res) => {
+  const url =
+    "https://api.openweathermap.org/data/2.5/weather?appid="+ process.env.WEATHER_API_KEY +"&q=moscow&units=metric&lang=ru";
 
-  })
+  https.get(url, function (response) {
+    console.log(response.statusCode, response.statusMessage); /// -> 200 OK
 
-  res.send("Hello!")
+    response.on("data", function (data) {
+      const weatherData = JSON.parse(data);
+      const { name } = weatherData;
+      const { temp, humidity } = weatherData.main;
+      const { description } = weatherData.weather[0];
+      const { icon } = weatherData.weather[0];
+      const iconUrl = "https://openweathermap.org/img/wn/" + icon + ".png";
+      const { speed } = weatherData.wind;
+
+      // res.send(`
+      //       <h3>Погода в Москве:${temp} °С, ${description}.</h3>
+      //       <img src=${iconUrl}>`);
+
+      res.send({ name, iconUrl, description, temp, humidity, speed });
+        
+    });
+  });
+
+  // res.send("Server is up!")
 });
-
 
 app.listen(port, () => {
   console.log(`Server on port http://localhost:${port}`);
---
+++ Node/weather-api-node/public/index.js
@@ -0,0 +1,25 @@
+let weather = {
+  fetchWeather: function () {
+    fetch("/weather")
+      .then((responce) => responce.json())
+      .then((data) => this.displayWeather(data));
+  },
+  displayWeather: function (data) {
+    console.log(data);
+
+    const { name, iconUrl, description, temp, humidity, speed } = data;
+
+    console.log(name, iconUrl, description, temp, humidity, speed);
+
+    document.querySelector(".city").innerText = `${name}`;
+    document.querySelector(".icon").src = iconUrl;
+    document.querySelector(".description").innerText = description;
+    document.querySelector(".temp").innerText = `${Math.round(temp)}°C`;
+    document.querySelector(".humidity").innerText = `влажность: ${humidity}%`;
+    document.querySelector(".wind").innerText = `скорость ветра: ${speed} м/с`;
+
+    document.querySelector(".weather").classList.remove("loading");
+  },
+};
+
+weather.fetchWeather();
