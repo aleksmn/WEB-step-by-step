902bdd1caeb9b3f65da3b203769abec825ddda17
aleksmn <al-mn@yandex.ru>
2022-07-11 14:57:41 +0300

    fix

--- WEB-lessons/lesson-26/craft-game.js
+++ WEB-lessons/lesson-26/craft-game.js
@@ -8,20 +8,21 @@ const craftBtn = document.getElementById("craft");
 craftBtn.onclick = function() {
 
   let winCount = 0;
+  let tries = 10;
 
-  for (let i = 1; i <= 10; i++) {
+  for (let i = 1; i <= tries; i++) {
     setTimeout(function () {
       if (Math.random() > 0.5) {
-        myDiv.innerHTML += `<p>Шаг ${i}: Удача!</p>`;
+        myDiv.innerHTML += `<p>Крафт ${i}: Удача!</p>`;
         winCount += 1;
       } else {
-        myDiv.innerHTML += `<p>Шаг ${i}: Неудача!</p>`;
+        myDiv.innerHTML += `<p>Крафт ${i}: Неудача!</p>`;
       }
     }, i * 1000);
   }
 
   setTimeout(function () {
-    myDiv.innerHTML += `<p><strong>Удачных попыток: ${winCount}</strong></p>`;
+    myDiv.innerHTML += `<p><strong>Удачных попыток: ${winCount} из ${tries} </strong></p>`;
   }, 11000);
 
 }
