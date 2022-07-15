15a4befeb7b5de2c680b5161646d03863e64e5e2
aleksmn <al-mn@yandex.ru>
2022-07-15 15:49:40 +0300

    init

--- CSS-frameworks/Bulma/script.js
+++ CSS-frameworks/Bulma/script.js
@@ -7,7 +7,7 @@ const navmenu = document.querySelector(".navbar-menu");
 
 burger.addEventListener("click", () => {
 
-  navmenu.classList.toggle("is-active")
+  navmenu.classList.toggle("is-active");
   burger.classList.toggle("is-active");
 
 })
