b63f761dd9365d4ca353d6a14446fde21711eaed
aleksmn <al-mn@yandex.ru>
2022-08-13 17:53:29 +0300

    add dark theme

--- WEB-lessons/lesson-25/script.js
+++ WEB-lessons/lesson-25/script.js
@@ -1,41 +1,35 @@
-const button1 = document.getElementById('btn-1');
-const button2 = document.getElementById('btn-2');
-
+const button1 = document.getElementById("btn-1");
+const button2 = document.getElementById("btn-2");
 
 button1.onclick = function () {
+  console.log(button1);
 
-    console.log(button1);
+  // текущий цвет:
+  let currColor = window.getComputedStyle(button1).backgroundColor;
 
-    // текущий цвет:
-    let currColor = window.getComputedStyle(button1).backgroundColor;
+  console.log(currColor);
 
-    console.log(currColor);
-    
-    // первый (зеленый) цвет:
-    const firstColor = "rgb(47, 129, 47)";
-    //второй (красный) цвет:
-    const secondColor = "rgb(207, 23, 23)";
+  // первый (зеленый) цвет:
+  const firstColor = "rgb(47, 129, 47)";
+  //второй (красный) цвет:
+  const secondColor = "rgb(207, 23, 23)";
 
-    // проверка цвета фона кнопки
-    if (currColor === secondColor) {
-
-        button1.style.backgroundColor = firstColor;
-        // document.getElementById("heading").style.color = "purple";
-        document.querySelector("img").style.display = "none";
-    }
-
-    else {
-        button1.style.backgroundColor = secondColor;
-        // document.getElementById("heading").style.color = "green";
-        document.querySelector("img").style.display = "block";
-    
-        // console.log("change to red");
-    }
-}
-
+  // проверка цвета фона кнопки
+  if (currColor == secondColor) {
+    button1.style.backgroundColor = firstColor;
+    // document.querySelector("body").style.backgroundColor = "black";
+    // document.querySelector(".wrapper").style.color = "white";
+    // document.getElementById("heading").style.color = "purple";
+    // document.querySelector("img").style.display = "none";
+  } else {
+    button1.style.backgroundColor = secondColor;
+    // document.querySelector("body").style.backgroundColor = "white";
+    // document.querySelector(".wrapper").style.color = "black";
+    // document.getElementById("heading").style.color = "green";
+    // document.querySelector("img").style.display = "block";
+  }
+};
 
 button2.onclick = function () {
-
-    button2.classList.toggle("bg-green");
-
-}
+  button2.classList.toggle("bg-green");
+};
