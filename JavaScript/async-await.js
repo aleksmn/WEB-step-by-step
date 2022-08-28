5577b9fdf872531c047815ddb9bb42a3db1ef4a5
aleksmn <al-mn@yandex.ru>
2022-08-28 15:40:18 +0300

    init

---
+++ JavaScript/async-await.js
@@ -0,0 +1,56 @@
+// Promise Async Await
+
+
+function resolveAfter2Seconds(x) {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve(x);
+        }, 2000);
+    });
+};
+
+async function add(x) {
+    console.log("Working.....")
+    setTimeout(() => {
+        console.log("Still working...")
+    }, 2000)
+    let a = await resolveAfter2Seconds(20);
+    let b = await resolveAfter2Seconds(30);
+    return x + a + b;
+};
+
+
+add(10).then(v => {
+    console.log(v) // prints 60 after 4 seconds.
+})
+
+
+
+// ------------------------------------------------
+
+// Example 1
+
+
+// function resolveAfter2Seconds(x) {
+//     setTimeout(() => {
+//         console.log("Working...")
+//     }, 2000);
+//     return x;
+// };
+
+
+// Example 2
+
+// function resolveAfter2Seconds(x) {
+//     return new Promise(resolve => {
+//         setTimeout(() => {
+//             resolve(x);
+//         }, 2000);
+//     });
+// };
+
+// resolveAfter2Seconds(22).then((v) => {
+//     console.log(v)
+// })
+
+
