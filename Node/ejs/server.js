3d3bce726647d94ba55097a5534d5680636c8556
aleksmn <al-mn@yandex.ru>
2022-10-07 13:43:28 +0300

    init

---
+++ Node/ejs/server.js
@@ -0,0 +1,46 @@
+const express = require("express"),
+    app = express();
+
+app.set("view engine", "ejs");
+
+app.get("/", function (req, res) {
+
+    let users = [
+        {
+            name: 'Александр',
+            designation: "учитель информатики"
+        },
+        {
+            name: 'Дмитрий',
+            designation: "ученик"
+        },
+        {
+            name: 'Мария',
+            designation: "учитель физики"
+        },
+        {
+            name: 'Владислав',
+            designation: "ученик"
+        },
+        {
+            name: 'Георгий',
+            designation: "ученик"
+        }
+    ];
+ 
+    let subheading = "Список пользователей нашего сайта"
+
+    res.render("index", {
+        users: users,
+        subheading: subheading
+    });
+
+});
+
+app.get("/about", function (req, res) {
+    res.render("about");
+});
+
+app.listen(3000, function () {
+    console.log("Server is running on port 3000 ");
+});
