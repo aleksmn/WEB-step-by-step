1ddcfa29355efa9b93fcbef8b028dc3ebeeb3c57
aleksmn <al-mn@yandex.ru>
2022-10-17 19:53:44 +0300

    init

---
+++ WEB-lessons/lesson-20/script.js
@@ -0,0 +1,18 @@
+// Логические операции
+
+// let username = prompt("Введите имя: ");
+
+
+// if (username == "Admin") {
+//     alert("Добро пожаловать, Админ!")
+//     console.log("На сайт зашел Админ")
+// }
+
+// else {
+//     alert("Привет " + username)
+//     console.log("На сайт зашел пользователь с ником " + username)
+// }
+
+
+
+
