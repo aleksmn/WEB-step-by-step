fcbf1e3e58d8451d81a02f66239d71f67b045918
aleksmn <al-mn@yandex.ru>
2022-12-09 15:17:50 +0300

    fix

--- JavaScript/NotesApp/script-0.js
+++ JavaScript/NotesApp/script-0.js
@@ -9,39 +9,67 @@ function saveNotes(notes) {
   localStorage.setItem("mynotes", JSON.stringify(notes));
 }
 
-console.log(getNotes());
+// Тестирование:
 
-function createNoteElement(id, content) {
-  const element = document.createElement("textarea");
-  element.classList.add("note");
-  element.value = content;
-  element.placeholder = "Новая заметка!";
+testNotes = [
+  { "id": 1, "content": "Привет из первой заметки!" },
+  { "id": 2, "content": "Это вторая заметка" },
+  { "id": 3, "content": "Третья заметка" }
+]
 
-  element.addEventListener("change", () => {
-    updateNote(id, element.value);
-  });
 
-  element.addEventListener("dblclick", () => {
-    const doDelete = confirm("Удалить заметку?");
+saveNotes(testNotes)
 
-    if (doDelete) {
-      deleteNote(id, element);
-    }
-  });
+// console.log(getNotes());
 
-  return element;
-}
 
-function addNote() {
-  console.log("Добавляем заметку...");
-}
 
-function updateNote(id, newContent) {
-  console.log("Изменяем заметку...");
-  console.log(id, newContent);
-}
 
-function deleteNote(id, element) {
-  console.log("Удаляем заметку...");
-  console.log(id);
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function createNoteElement(id, content) {
+//   const element = document.createElement("textarea");
+//   element.classList.add("note");
+//   element.value = content;
+//   element.placeholder = "Новая заметка!";
+
+//   element.addEventListener("change", () => {
+//     updateNote(id, element.value);
+//   });
+
+//   element.addEventListener("dblclick", () => {
+//     const doDelete = confirm("Удалить заметку?");
+
+//     if (doDelete) {
+//       deleteNote(id, element);
+//     }
+//   });
+
+//   return element;
+// }
+
+// function addNote() {
+//   console.log("Добавляем заметку...");
+// }
+
+// function updateNote(id, newContent) {
+//   console.log("Изменяем заметку...");
+//   console.log(id, newContent);
+// }
+
+// function deleteNote(id, element) {
+//   console.log("Удаляем заметку...");
+//   console.log(id);
+// }
