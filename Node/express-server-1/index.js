bb23741d1cccb39c22cc94cd174972720e26895c
aleksmn <al-mn@yandex.ru>
2023-04-14 15:51:05 +0300

    add pages

--- Node/express-server-1/index.js
+++ Node/express-server-1/index.js
@@ -3,15 +3,17 @@ const app = express();
 const port = 3050;
 
 app.get("/", (req, res) => {
+
   res.sendFile(__dirname + "/index.html");
+
 });
 
 app.get("/contacts", (req, res) => {
-  res.send("<h1>Contact Us!</h1>");
+  res.sendFile(__dirname + "/contacts.html")
 });
 
 app.get("/about", (req, res) => {
-  res.send("<h1>About Page</h1>");
+  res.sendFile(__dirname + "/about.html")
 });
 
 app.listen(port, () => {
