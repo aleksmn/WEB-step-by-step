ec2b92d24bc819c3f73d5c58d2025d4d27bf8f20
aleksmn <al-mn@yandex.ru>
2023-05-05 10:39:41 +0300

    fix

--- Node/express-server-0/index.js
+++ Node/express-server-0/index.js
@@ -4,20 +4,22 @@ const port = 3050;
 
 app.get("/", (req, res) => {
   res.send("<h1>Hello World!</h1>");
-  // console.log(req);
 });
 
+
 app.get("/contacts", (req, res) => {
-  res.send("<h1>Contact Us! Hello!</h1>");
+  res.send("<h1>Contact Us!</h1>");
 });
+
 
 app.get("/about", (req, res) => {
   res.send("<h1>About Page</h1>");
 });
 
+
 app.listen(port, () => {
   
   console.log(
-    `Example app listening on port ${port}`
+    'Example app listening on port http://localhost:' + port
   );
 });
