cbdc40782c4aaad5417e69ea74555bf066828c5b
aleksmn <al-mn@yandex.ru>
2023-06-05 15:40:10 +0300

    init

---
+++ WEB-lessons/lesson-32_u/script.js
@@ -0,0 +1,79 @@
+// function print_contact() {
+//     alert(' skype: helper \n telegram: @helper \n tel: 8995345235 ')
+// }
+
+
+
+// function randomElement(arr) {
+//     const rand = Math.floor(Math.random() * arr.length);
+//     return arr[rand];
+// }
+
+// Проверяем работу функции:
+// console.log(randomElement(['apple', 'orange', 'tomato']))
+
+
+function rand_tshirt() {
+
+    // Массив картинок: 
+    const arr = ["images/tshirt-1.jpg", "images/tshirt-2.jpg", "images/tshirt-3.jpg"];
+
+    // Подбираем первую картинку
+    let rand = Math.floor(Math.random() * arr.length);
+    // Меняем 1 картинку
+    document.getElementById("ts-image-1").src = arr.splice(rand, 1)[0]
+
+    // Подбираем вторую картинку
+    rand = Math.floor(Math.random() * arr.length);
+    // Меняем 2 картинку
+    document.getElementById("ts-image-2").src = arr.splice(rand, 1)[0]
+
+}
+
+
+// Модальные окна
+
+const modalGift = document.getElementById("giftModal");
+const openBtnGift = document.getElementById("giftBtn");
+const closeBtnGift = document.getElementById("giftClose");
+
+// console.log(modalGift, openBtnGift, closeBtnGift)
+
+openBtnGift.onclick = function () {
+    modalGift.style.display = "block";
+}
+
+closeBtnGift.onclick = function () {
+    modalGift.style.display = "none";
+}
+
+modalGift.onclick = function (event) {
+    if (event.target === modalGift) {
+        modalGift.style.display = "none";
+
+    }
+}
+
+
+
+const modalTp = document.getElementById("tpModal");
+const openBtnTp = document.getElementById("tpBtn");
+const closeBtnTp = document.getElementById("tpClose");
+
+
+
+openBtnTp.onclick = function () {
+    modalTp.style.display = "block";
+}
+
+closeBtnTp.onclick = function () {
+    modalTp.style.display = "none";
+}
+
+
+modalTp.onclick = function (event) {
+    if (event.target === modalTp) {
+        modalTp.style.display = "none";
+
+    }
+}
---
+++ WEB-lessons/lesson-32_u/site-rating.js
@@ -0,0 +1,84 @@
+let siteRating = 0;
+
+function star(starNumber) {
+    // starNumber - номер звезды на которую навел пользователь
+
+    siteRating = starNumber;
+
+    starNumber = starNumber - 1;
+
+    // console.log(starNumber);
+
+    let ids = ["star-1", "star-2", "star-3", "star-4", "star-5"];
+
+    // console.log(ids[starNumber]);
+
+    let star = document.getElementById(ids[starNumber]);
+
+    // console.log(star);
+
+    // // // // если звезда пустая
+    if (star.src.includes("images/star_2.png")) {
+
+        // то меняем ее и все предыдущие на заполненные
+        for (let i = 0; i <= starNumber; i++) {
+            let id = ids[i];
+            document.getElementById(id).src = "images/star_1.png";
+        }
+
+    }
+
+    // // // иначе - звезда заполена
+    else {
+
+        for (let i = 0; i < ids.length; i++) {
+            if (i > starNumber) {
+                id = ids[i];
+                document.getElementById(id).src = "images/star_2.png";
+
+            }
+        }
+    }
+}
+
+function resetRating() {
+
+    siteRating = 0;
+
+    // сбросить рейтинг
+    let ids = ["star-1", "star-2", "star-3", "star-4", "star-5"];
+
+    for (let i = 0; i < ids.length; i++) {
+        let id = ids[i];
+        document.getElementById(id).src = "images/star_2.png";
+    }
+}
+
+const sendRatingBtn = document.getElementById("sendRating");
+
+sendRatingBtn.addEventListener("click", function () {
+    let text;
+
+    switch(siteRating) {
+        case 1:
+        case 2:
+        case 3:
+            text = "Спасибо за ваш отзыв!";
+            console.log("Вывести модальное окно для отзыва!");
+            break;
+        case 4:
+            text = "Спасибо за хороший балл!";
+            break;
+        case 5:
+            text = "Спасибо за высокую оценку :)";
+            break;
+
+        default:
+            text = "Пожалуйста, поставьте оценку от 1 до 5";
+            break;
+    }
+
+
+    document.getElementById("ratingText").innerText = text;
+
+});
