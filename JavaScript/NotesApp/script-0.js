1d7e603cd3428768f17d1d104a579f1e16885455
aleksmn <al-mn@yandex.ru>
2023-06-20 09:56:25 +0300

    fix

--- JavaScript/NotesApp/script-0.js
+++ JavaScript/NotesApp/script-0.js
@@ -1,73 +1,87 @@
 const notesContainer = document.getElementById("app");
 const addNoteButton = notesContainer.querySelector(".add-note");
 
+
+// Функция для получение заметок из локального хранилища
 function getNotes() {
   return JSON.parse(localStorage.getItem("mynotes") || "[]");
 }
 
+console.log(getNotes())
+
+
+
+// Функция для сохранения заметок в локальном хранилище
 function saveNotes(notes) {
   localStorage.setItem("mynotes", JSON.stringify(notes));
 }
 
-// Тестирование:
 
+
+
+// // Тестирование:
+// Создаем список заметок в формате JSON
 testNotes = [
   { "id": 1, "content": "Привет из первой заметки!" },
   { "id": 2, "content": "Это вторая заметка" },
   { "id": 3, "content": "Третья заметка" }
 ]
 
-
+// Сохраняем заметки
 saveNotes(testNotes)
 
-console.log(getNotes());
-
-
-function createNoteElement(id, content) {
-  const element = document.createElement("textarea");
-  element.classList.add("note");
-  element.value = content;
-  element.placeholder = "Новая заметка!";
-
-  element.addEventListener("change", () => {
-    updateNote(id, element.value);
-  });
-
-  element.addEventListener("dblclick", () => {
-    const doDelete = confirm("Удалить заметку?");
-
-    if (doDelete) {
-      deleteNote(id, element);
-    }
-  });
-
-  return element;
-}
-
-
-function addNote() {
-  console.log("Добавляем заметку...");
-}
-
-function updateNote(id, newContent) {
-  console.log("Изменяем заметку...");
-  console.log(id, newContent);
-}
-
-function deleteNote(id, element) {
-  console.log("Удаляем заметку...");
-  console.log(id);
-}
 
 
 
-// Выводим заметки на страницу
-getNotes().forEach(note => {
-  const noteElement = createNoteElement(note.id, note.content);
-  notesContainer.insertBefore(noteElement, addNoteButton);
-});
 
-// Кнопка добавления заметки (+)
-addNoteButton.addEventListener("click", () => addNote());
+// console.log(getNotes());
+
+
+// function createNoteElement(id, content) {
+//   const element = document.createElement("textarea");
+//   element.classList.add("note");
+//   element.value = content;
+//   element.placeholder = "Новая заметка!";
+
+//   element.addEventListener("change", () => {
+//     updateNote(id, element.value);
+//   });
+
+//   element.addEventListener("dblclick", () => {
+//     const doDelete = confirm("Удалить заметку?");
+
+//     if (doDelete) {
+//       deleteNote(id, element);
+//     }
+//   });
+
+//   return element;
+// }
+
+
+// function addNote() {
+//   console.log("Добавляем заметку...");
+// }
+
+// function updateNote(id, newContent) {
+//   console.log("Изменяем заметку...");
+//   console.log(id, newContent);
+// }
+
+// function deleteNote(id, element) {
+//   console.log("Удаляем заметку...");
+//   console.log(id);
+// }
+
+
+
+// // Выводим заметки на страницу
+// getNotes().forEach(note => {
+//   const noteElement = createNoteElement(note.id, note.content);
+//   notesContainer.insertBefore(noteElement, addNoteButton);
+// });
+
+// // Кнопка добавления заметки (+)
+// addNoteButton.addEventListener("click", () => addNote());
 
 
