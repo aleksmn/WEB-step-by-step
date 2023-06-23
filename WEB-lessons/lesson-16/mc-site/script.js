9ed95cdbf9ad971effdd93909c6d3158246aa701
aleksmn <al-mn@yandex.ru>
2023-06-23 11:53:38 +0300

    fix

--- WEB-lessons/lesson-16/mc-site/script.js
+++ WEB-lessons/lesson-16/mc-site/script.js
@@ -1,23 +1,24 @@
-arrows = document.querySelectorAll(('.slider-image .arrow'))
+const arrows = document.querySelectorAll('.slider-image .arrow');
 
-// console.log(arrows[0], arrows[1])
+console.log(arrows[0], arrows[1]);
 
-prev_image_arrow = arrows[0]
-next_image_arrow = arrows[1]
+const prev_image_arrow = arrows[0];
+const next_image_arrow = arrows[1];
 
-mob_images = ['mob_1.jpg', 'mob_2.png', 'mob_3.png']
-mob_names = ['Эндермен', 'Скелет', 'Зомби']
-mob_texts = ['Большой и злой игровой моб в Minecraft. Его тяжело убить и он умеет телепортироваться',
+const mob_images = ['mob_1.jpg', 'mob_2.png', 'mob_3.png'];
+const mob_names = ['Эндермен', 'Скелет', 'Зомби'];
+const mob_texts = ['Большой и злой игровой моб в Minecraft. Его тяжело убить и он умеет телепортироваться',
     'Скелет  — распространённый враждебный моб, являющийся нежитью и вооружённый луком',
-    'Зомби - неживые враждебные мобы, атакующие игроков и деревенских жителей, горящие на солнце']
+    'Зомби - неживые враждебные мобы, атакующие игроков и деревенских жителей, горящие на солнце'];
 
-current_image = 0
+let current_image = 0;
 
 prev_image_arrow.onclick = function () {
     console.log('Предыдущее изображение')
     change_image(-1)
 
 }
+
 next_image_arrow.onclick = function () {
     console.log('Следующее изображение')
     change_image(1)
@@ -25,15 +26,18 @@ next_image_arrow.onclick = function () {
 
 function change_image(n) {
 
-    current_image += n
+    current_image += n;
 
-    if (current_image < 0) { current_image = mob_images.length - 1 }
-    if (current_image > (mob_images.length - 1)) { current_image = 0 }
+    // if (current_image < 0) { current_image = mob_images.length - 1 }
+    // if (current_image > (mob_images.length - 1)) { current_image = 0 }
 
-    console.log(current_image)
+    current_image = current_image % mob_images.length
 
-    document.querySelector('.slider-image img').src = 'images/' + mob_images[current_image]
-    document.querySelector('.slider .description-name').innerText = mob_names[current_image]
-    document.querySelector('.slider .description-text').innerText = mob_texts[current_image]
+    console.log(current_image);
+
+
+    document.querySelector('.slider-image img').src = 'images/' + mob_images[current_image];
+    document.querySelector('.slider .description-name').innerText = mob_names[current_image];
+    document.querySelector('.slider .description-text').innerText = mob_texts[current_image];
 
 }
