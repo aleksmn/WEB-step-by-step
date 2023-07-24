72cd8132529ea493f2b7f57aa80663fa0846c0da
aleksmn <al-mn@yandex.ru>
2023-07-24 10:51:49 +0300

    add info

--- Node/spa-params/static/js/views/Contacts.js
+++ Node/spa-params/static/js/views/Contacts.js
@@ -11,6 +11,11 @@ export default class extends AbstractView {
       <h1>Контакты</h1>
 
       <p>Здесь вы найдете мои контактные данные!</p>
+
+        <a href="#"><i class="fa-brands fa-telegram"></i> Телеграм</a><br>
+        <a href="#"><i class="fa-brands fa-discord"></i> Дискорд</a><br>
+        <a href="#"><i class="fa-solid fa-envelope"></i> example@example.com</a><br>
+
       <p>
         <a href="/" data-link>Перейти на главную страницу</a>
       </p>
