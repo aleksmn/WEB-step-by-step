dadb2b36cab0b35d882accba3097998fd9b16ddb
aleksmn <al-mn@yandex.ru>
2023-08-10 14:02:54 +0300

    fix

--- JavaScript/JS-first-steps/test.js
+++ JavaScript/JS-first-steps/test.js
@@ -1,4 +1,33 @@
 
+
+
+// Создаем функцию
+
+function sayHello() {
+
+    // Объявляем переменную username
+    let username = prompt("Введите имя: ")
+
+    // Если username равен 'Admin'
+    if (username == 'Admin') {
+        alert("Добро пожаловать, " + username + "!")
+    }
+
+    else {
+        alert("Привет " + username + "!")
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
 function sumNums(a, b) {
 
     //parseInt() - получить целое число
