8a7de780f28e2b5a3aba8bd2dbd9b7d301995c06
aleksmn <al-mn@yandex.ru>
2023-08-10 14:54:42 +0300

    init

---
+++ WEB-lessons/lesson-32_u/test.js
@@ -0,0 +1,97 @@
+
+
+
+// Создаем функцию
+
+function sayHello() {
+
+    // Объявляем переменную username
+    let username = prompt("Введите имя: ")
+
+    // Если username равен 'Admin'
+    if (username == 'Admin') {
+        alert("Добро пожаловать, " + username + "!")
+    }
+
+    else {
+        alert("Привет " + username + "!")
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+function sumNums(a, b) {
+
+    //parseInt() - получить целое число
+    //parseFloat() - получить дробное число 
+
+    alert(a + b)
+
+}
+
+function floorDiv(a, b) {
+
+    const remainder = a % b;
+
+    alert((a - remainder) / b)
+}
+
+
+
+
+
+function calc() {
+    alert("Добро пожаловать в Калькулятор!")
+    const choice = prompt("Выберите действие:\n1 - сумма\n2 - целочисленное деление")
+
+    const a = parseFloat(prompt("Введите число: "));
+    const b = parseFloat(prompt("Введите второе число:"));
+
+    if (choice === '1') {
+        sumNums(a, b)
+    }
+
+    else if (choice === '2') {
+        floorDiv(a, b)
+
+    }
+
+    // добавить * / - 
+
+}
+
+// Калькулятор - вызов
+calc()
+
+
+
+//// Строки
+
+// const firstName = 'Владимир';
+// const job = 'программист';
+// const birthYear = 1995;
+
+// console.log(firstName)
+// console.log(firstName.length)
+// // Первая буква
+// console.log(firstName[0])
+// // Последний символ
+// console.log(firstName[firstName.length - 1])
+
+// console.log(firstName.indexOf('Влад'))
+
+
+
+
+
+// Задача на дом
+// Написать функцию, которая принимает от пользователя 2 числа и выводит
+// результат деления нацело и остаток от деления
