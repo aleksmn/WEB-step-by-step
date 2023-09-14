5b853c75470d987545b09d65e0200315b099f6a7
aleksmn <al-mn@yandex.ru>
2023-09-14 16:52:12 +0300

    init

---
+++ Node/01.js
@@ -0,0 +1,23 @@
+// Объявление переменной в JS
+
+// var a = 5;
+
+let b = 0;
+console.log(b)
+
+if (!b) {
+    b = 10;
+}
+
+b++;
+
+console.log(b)
+
+
+// Константы
+
+let username = "Витя";
+
+username = "Aleksander"  
+
+console.log(username)
