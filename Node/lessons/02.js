2fa936a4ebc97e890d5fc38ba4a677021b13be35
aleksmn <al-mn@yandex.ru>
2023-09-18 16:47:10 +0300

    init

---
+++ Node/lessons/02.js
@@ -0,0 +1,44 @@
+// Целочисленная арифметика в JS
+
+// Остаток от деления
+const num1 = 18;
+const num2 = 7;
+
+const remainder = num1 % num2;
+console.log(`Остаток от деления ${num1} на ${num2} равен ${remainder}`)
+
+
+
+
+
+
+
+
+// const testNum = 2134230;
+// Если число четное, вывести "Тестовое число четное"
+// иначе, вывести "Тестовое число НЕ четное"
+
+// if (testNum % 2 === 0) {
+//     console.log("Тестовое число четное")
+// }
+// else {
+//     console.log("Тестовое число НЕ четное")
+// }
+
+
+// console.log(!true)
+
+
+
+// const num1 = -15;
+// const num2 = -6;
+
+// const remainder = num1 % num2;
+
+// console.log(remainder);
+
+
+
+// const floorDiv = (num1 - remainder) / num2;
+
+// console.log(floorDiv);
