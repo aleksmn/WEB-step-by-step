68435c6e16c2cadef535da7c591807a7e3460c1e
aleksmn <al-mn@yandex.ru>
2024-02-02 13:52:31 +0300

    fix

--- minecraft-site/script.js
+++ minecraft-site/script.js
@@ -1,39 +1,50 @@
 const arrows = document.querySelectorAll('.slider-image .arrow');
 
-console.log(arrows[0], arrows[1]);
 
 const prev_image_arrow = arrows[0];
 const next_image_arrow = arrows[1];
 
+// создадим массивы для карточек с мобами
 const mob_images = ['mob_1.jpg', 'mob_2.png', 'mob_3.png'];
 const mob_names = ['Эндермен', 'Скелет', 'Зомби'];
 const mob_texts = ['Большой и злой игровой моб в Minecraft. Его тяжело убить и он умеет телепортироваться',
-    'Скелет  — распространённый враждебный моб, являющийся нежитью и вооружённый луком',
-    'Зомби - неживые враждебные мобы, атакующие игроков и деревенских жителей, горящие на солнце'];
+    'Распространённый враждебный моб, являющийся нежитью и вооружённый луком',
+    'Неживые враждебные мобы, атакующие игроков и деревенских жителей, горящие на солнце'];
+
+
 
 let current_image = 0;
 
 prev_image_arrow.onclick = function () {
-    console.log('Предыдущее изображение')
+    console.log('Prev Image')
     change_image(-1)
 
 }
 
 next_image_arrow.onclick = function () {
-    console.log('Следующее изображение')
+    console.log('Next Image')
     change_image(1)
 }
+
+
 
 function change_image(n) {
 
     current_image += n;
 
-    // if (current_image < 0) { current_image = mob_images.length - 1 }
-    // if (current_image > (mob_images.length - 1)) { current_image = 0 }
+    if (current_image < 0) { 
+        current_image = mob_images.length - 1 
+    }
 
-    current_image = (current_image + mob_images.length) % mob_images.length
+    if (current_image > (mob_images.length - 1)) {
+        current_image = 0 
+    }
+
+    // current_image = (current_image + mob_images.length) % mob_images.length
 
     console.log(current_image);
+
+
 
 
     document.querySelector('.slider-image img').src = 'images/' + mob_images[current_image];
