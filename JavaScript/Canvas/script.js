d935a82b3b36d8f94373b8febe0b5f4e15c6f370
aleksmn <al-mn@yandex.ru>
2024-03-18 13:18:08 +0300

    init

---
+++ JavaScript/Canvas/script.js
@@ -0,0 +1,15 @@
+const map = document.getElementById("myCanvas");
+const canvas = map.getContext("2d");
+
+canvas.fillStyle = "#58A8D8";
+
+canvas.fillRect(200, 200, 440, 320);
+
+
+// canvas.fillStyle = "#58A8D8";
+
+// canvas.beginPath();
+// canvas.moveTo(200, 200);
+// canvas.lineTo(500, 350);
+// canvas.lineTo(200, 500);
+// canvas.fill();
