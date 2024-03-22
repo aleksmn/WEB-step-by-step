697d413fe936aebafef7ab3ebb08f2022e3c3dc3
aleksmn <al-mn@yandex.ru>
2024-03-22 14:13:34 +0300

    init

--- JavaScript/Canvas/script.js
+++ JavaScript/Canvas/script.js
@@ -1,15 +1,8 @@
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
 
-ctx.fillStyle = "#58A8D8";
+ctx.fillStyle = "rgb(32, 227, 214)";
 
 ctx.fillRect(200, 200, 440, 320);
 
 
-// ctx.fillStyle = "#58A8D8";
-
-// ctx.beginPath();
-// ctx.moveTo(200, 200);
-// ctx.lineTo(500, 350);
-// ctx.lineTo(200, 500);
-// ctx.fill();
