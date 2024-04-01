49b96f7bc29a7c47f79dbf408fabfa762bc8d50c
aleksmn <al-mn@yandex.ru>
2024-04-01 16:49:29 +0300

    change size

--- JavaScript/Canvas/script.js
+++ JavaScript/Canvas/script.js
@@ -1,50 +1,52 @@
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
 
-ctx.fillStyle = "rgb(32, 227, 214)";
+// Объявим цвет заливки
+ctx.fillStyle = "aqua";
 
-// Прямоугольник fillRect(x, y, width, height)
-// ctx.fillRect(200, 200, 440, 320);
+// Цвет линии
+ctx.strokeStyle = "aqua";
 
-// ctx.fillStyle = "rgb(100, 100, 220)";
-// ctx.fillRect(100, 100, 440, 320);
+// Прямоугольник 
+// ctx.fillRect(x, y, width, height);
+
+// const window_width = 600;
+// const window_height = 600;
+
+// const rect_width = 100;
+// const rect_height = 100;
+
+// ctx.fillRect(window_width / 2 - rect_width/2, window_height / 2 - rect_height/2, rect_width, rect_height);
 
 
 // Линия
-// ctx.moveTo(0, 0);
-// ctx.lineTo(1000, 600);
-// ctx.stroke();
+// ctx.moveTo(0, 0)
+// ctx.lineTo(600, 600)
+// ctx.stroke()
 
 
-// Треугольник
+// // Треугольник
 // ctx.beginPath();
-// ctx.moveTo(200, 100)
-// ctx.lineTo(500, 250)
-// ctx.lineTo(200, 400)
+// ctx.moveTo(100, 100);
+// ctx.lineTo(300, 250);
+// ctx.lineTo(100, 400);
 // ctx.fill()
 
+
 // ctx.beginPath();
-// ctx.moveTo(800, 100)
-// ctx.lineTo(500, 250)
-// ctx.lineTo(800, 400)
+// ctx.moveTo(500, 100);
+// ctx.lineTo(300, 250);
+// ctx.lineTo(500, 400);
 // ctx.fill()
 
 
 // Толстые линии 
 // ctx.lineWidth = 30;
-// ctx.strokeStyle = "rgb(32, 227, 214)"
-
 // ctx.beginPath();
-// ctx.moveTo(200, 100)
-// ctx.lineTo(500, 250)
-// ctx.lineTo(200, 400)
-
-// ctx.moveTo(800, 100)
-// ctx.lineTo(500, 250)
-// ctx.lineTo(800, 400)
-
-// ctx.stroke();
-
+// ctx.moveTo(100, 100);
+// ctx.lineTo(300, 250);
+// ctx.lineTo(100, 400);
+// ctx.stroke()
 
 
 // Круг
@@ -56,11 +58,10 @@ ctx.fillStyle = "rgb(32, 227, 214)";
 // ctx.stroke()
 
 
-
 // Кривые линии
-// ctx.lineWidth = 30;
-// ctx.strokeStyle = "rgb(32, 227, 214)"
-// ctx.beginPath();
-// ctx.moveTo(200, 300);
-// ctx.quadraticCurveTo(500, 400, 800, 300);
-// ctx.stroke();
+ctx.lineWidth = 30;
+ctx.strokeStyle = "rgb(32, 227, 214)"
+ctx.beginPath();
+ctx.moveTo(100, 300);
+ctx.quadraticCurveTo(250, 400, 500, 300);
+ctx.stroke();
