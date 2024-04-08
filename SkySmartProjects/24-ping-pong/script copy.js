ca3119791f7e07ece7f7bab19237658269529a5e
aleksmn <al-mn@yandex.ru>
2024-04-08 17:03:17 +0300

    fix

--- SkySmartProjects/24-ping-pong/script copy.js
+++ SkySmartProjects/24-ping-pong/script copy.js
@@ -37,7 +37,7 @@ const ball = {
     width: grid,
     height: grid,
     dx: ballSpeed,
-    dy: -ballSpeed,
+    dy: ballSpeed,
     isResetted: false
 }
 
@@ -74,6 +74,49 @@ function renderСanvas() {
 
 }
 
+// Движение ракеток
+function movePaddles() {
+    leftPaddle.y += leftPaddle.dy;
+    rightPaddle.y += rightPaddle.dy;
+}
+
+
+
+// Столкновение мяча со стенами
+function collideWallsWithBall() {
+    if (ball.y < grid) {
+        ball.y = grid
+        ball.dy = -ball.dy;
+    }
+
+    else if (ball.y > canvas.height - grid*2) {
+        ball.y = canvas.height - grid*2;
+        ball.dy = -ball.dy;
+    }
+}
+
+// Столкновения ракеток со стенами
+function collideWallsWithPaddle(paddle) {
+    if (paddle.y < grid) {
+        paddle.y = grid;
+    }
+    else if (paddle.y > maxPaddleY) {
+        paddle.y = maxPaddleY;
+    }
+}
+
+
+function isCollides(object1, object2) {
+    const width1 = object1.x + object1.width;
+    const width2 = object2.x + object2.width;
+    const height1 = object1.y + object1.height;
+    const height2 = object2.y + object2.height;
+    return object1.x < width2
+        && object2.x < width1
+        && object1.y < height2
+        && object2.y < height1;
+}
+
 
 // Основной цикл программы
 function loop() {
@@ -83,13 +126,35 @@ function loop() {
     renderPaddle(rightPaddle);
     renderBall();
     moveBall();
+    movePaddles();
+    collideWallsWithBall();
 
+    collideWallsWithPaddle(leftPaddle);
+    collideWallsWithPaddle(rightPaddle);
 
     // переключаем кадр (в конце)
     requestAnimationFrame(loop);
 }
 
 
+// Отслеживаем нажатия на кнопки
+document.addEventListener('keydown', (event) => {
+    console.log(event)
+    if (event.key == 'w' || event.key == 'ц') {
+        leftPaddle.dy = -paddleSpeed;
+    }
+    else if (event.key == "s" || event.key == 'ы') {
+        leftPaddle.dy = paddleSpeed;
+    }
+});
+
+document.addEventListener('keyup', (event) => {
+
+    if (event.key == 'w' || event.key == "s" || event.key == 'ы' || event.key == 'ы') {
+        leftPaddle.dy = 0;
+    }
+    
+});
 
 
 // запускаем цикл
