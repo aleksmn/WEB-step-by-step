758b55d390ff0e1d6e7091524486d70d8889b04a
aleksmn <al-mn@yandex.ru>
2024-04-12 14:50:28 +0300

    add snake and food

--- JavaScript/snake_game/script.js
+++ JavaScript/snake_game/script.js
@@ -18,3 +18,87 @@ let score = 0;
 let highScore = 0;
 
 highScoreElement.innerText = `High-score: ${highScore}`
+
+
+const updateFoodPosition = () => {
+    foodX = Math.floor(Math.random() * 30) + 1;
+    foodY = Math.floor(Math.random() * 30) + 1;
+}
+
+
+// Управление змейкой
+const changeDirection = e => {
+    if (e.key === "ArrowUp" && velocityY != 1) {
+        velocityX = 0;
+        velocityY = -1;
+    }
+
+    else if (e.key === "ArrowDown" && velocityY != -1) {
+        velocityX = 0;
+        velocityY = 1;
+    }
+
+    else if (e.key === "ArrowLeft" && velocityX != 1) {
+        velocityX = -1;
+        velocityY = 0;
+    }
+
+    else if (e.key === "ArrowRight" && velocityX != -1) {
+        velocityX = 1;
+        velocityY = 0;
+    }
+
+}
+
+// События на кнопки под экраном
+controls.forEach(button => button.addEventListener("click", () => changeDirection({
+    key: button.dataset.key 
+})));
+
+
+const initGame = () => {
+
+
+    let html = `<div class="food" style="grid-area:${foodY} / ${foodX}"></div>`;
+
+    // Кагда съедаем еду, позиция меняется на случайную
+    if (snakeX === foodX && snakeY === foodY) {
+        updateFoodPosition();
+        // Добавляем элемент к змейке
+        snakeBody.push([foodX, foodY]);
+
+        // Увеличиваем счет на 1
+        score++;
+
+        // Увеличиваем highscore
+        // ...
+
+        scoreElement.innerText = `Score: ${score}`;
+         
+    }
+
+    // Меняем положение змеи
+    // Голово
+    snakeX += velocityX;
+    snakeY += velocityY;
+    // Хвост
+    for (let i = snakeBody.length - 1; i > 0; i--) {
+        snakeBody[i] = snakeBody[i - 1];
+    }
+    snakeBody[0] = [snakeX, snakeY];
+
+
+
+    // Создадим div для каждой части змейки
+    for (let i = 0; i < snakeBody.length; i++) {
+        html += `<div class="head" style="grid-area:${snakeBody[i][1]} / ${snakeBody[i][0]}"></div>`;
+
+    }
+
+    playBoard.innerHTML = html;
+
+}
+
+// Запуск
+updateFoodPosition();
+intervalId = setInterval(initGame, 100);
