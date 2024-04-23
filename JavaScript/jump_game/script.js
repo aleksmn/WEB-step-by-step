5a18e53614dfac07683b22f6e14108c5751520a2
aleksmn <al-mn@yandex.ru>
2024-04-23 21:44:21 +0300

    init

--- JavaScript/jump_game/script.js
+++ JavaScript/jump_game/script.js
@@ -19,34 +19,91 @@ const dragon = {
 
 }
 
+const fire = {
+    x: 500,
+    y: canvas.height - 50,
+    width: 50,
+    height: 50,
+    speed: 2
+}
+
 
 function drawDragon() {
     ctx.drawImage(dragonImg, dragon.x, dragon.y, dragon.width, dragon.height)
 }
 
-function jumpDragon () {
+
+function drawFire() {
+    ctx.drawImage(fireImg, fire.x, fire.y, fire.width, fire.height)
+}
+
+
+function moveFire() {
+    if (fire.x > -50) {
+        fire.x -= fire.speed
+    }
+    else {
+        fire.x = canvas.width
+    }
+}
+
+
+
+function jumpDragon() {
     // Прыжок вверх
-    if (dragon.jumping && !dragon.highestJump && dragon.y > 50) {
+    if (dragon.jumping && !dragon.highestJump && dragon.y > 30) {
         dragon.y -= dragon.jumpSpeed
     } 
     // спуск вниз
-    else if 
+    else if (dragon.y < canvas.height - dragon.height) {
+        dragon.highestJump = true
+        dragon.y += dragon.jumpSpeed
+        
+    }
+
+    else {
+        dragon.highestJump = false
+        dragon.jumping = false
+    }
 
 }
 
+// Столкновения
+function collision() {
+    if (
+        dragon.x < fire.x &&
+        dragon.x + 40 > fire.x &&
+        dragon.y + 80 > fire.y
+    ) {
+        clearInterval(interval)
+    }
+}
 
 
 function loop() {
     ctx.clearRect(0, 0, canvas.width, canvas.height)
     drawDragon()
+    drawFire()
+    jumpDragon()
+    moveFire()
+    collision()
 }
-
-
 
 // Старт игры
 
 function startGame() {
-    interval = setInterval(loop, 100)
+    interval = setInterval(loop, 1000 / 160)
 }
 
+// События
+
+document.addEventListener("keydown", function() {
+    if (!dragon.jumping) {
+        dragon.jumping = true;
+    }
+})
+
+
 startGame()
+
+
