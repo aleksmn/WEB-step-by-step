9cd7fd5e754201ff330494983fcf46d868d35320
aleksmn <al-mn@yandex.ru>
2024-10-24 14:32:15 +0300

    fix

--- Node/nodejs_programming/loops.js
+++ Node/nodejs_programming/loops.js
@@ -1,3 +1,32 @@
+
+for (let i=1; i<=10; i++) {
+
+    if (i==4) {
+        continue;
+    }
+
+    if (i==7) {
+        break;
+    }
+
+    console.log(i);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // 1) Найти сумму чисел от 0 до 100 включительно
 // 2) Среди чисел от 0 до 100, найти сумму чисел, кратных 5
 // 3) Среди чисел от 0 до 100, найти сумму чисел, НЕ кратных 5
@@ -10,18 +39,18 @@ let endNum = 100;
 
 for (let i=startNum; i<=endNum; i++) {
 
-    sum1 += i;
+    sum1 = sum1 + i;
     
     if (i % 5 == 0) {
         sum2 = sum2 + i;
         continue
     }
     
-    console.log(i)
+    // console.log(i)
     sum3 = sum3 + i;
 }
 
 
-console.log(sum1);
-console.log(sum2);
-console.log(sum3);
+// console.log(sum1);
+// console.log(sum2);
+// console.log(sum3);
