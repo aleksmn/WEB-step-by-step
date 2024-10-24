e127be30625cf923b657b36e308d98ff564b8955
aleksmn <al-mn@yandex.ru>
2024-10-24 14:55:15 +0300

    init

---
+++ Node/nodejs_programming/01_data_types.js
@@ -0,0 +1,42 @@
+// Number
+let answer = 42;
+let pi = 3.14;
+
+console.log(answer * pi)
+
+// String
+let greeting1 = "Hello, ";
+let greeting2 = "world!";
+
+console.log(greeting1 + greeting2)
+
+// Boolean
+
+let checkNum = answer > 0;
+
+console.log(checkNum);
+
+console.log(5 == "5");
+console.log(5 < "6");
+
+
+// Undefined
+let lastName;
+let firstName = undefined;
+
+console.log(lastName, firstName)
+
+
+
+// Array
+let colors = ["red", "green", "blue"];
+let mixed = ["red", 1, true, [true, false]];
+let empty = []
+
+console.log(mixed[3][1])
+
+// Object
+let person = { name: "John Doe", age: 30, gender: "Male" };
+
+console.log(person.name);
+console.log(person["age"]);
---
+++ Node/nodejs_programming/02_logic.js
@@ -0,0 +1,15 @@
+
+// Logical Operators: AND, OR, NOT
+
+let myFavoriteNumber = 7;
+// AND
+console.log(myFavoriteNumber > 0 && myFavoriteNumber <= 9)
+console.log(myFavoriteNumber > 0 && myFavoriteNumber <= 5)
+
+// OR
+console.log(myFavoriteNumber > 0 || myFavoriteNumber <= 9)
+console.log(myFavoriteNumber == 1 || myFavoriteNumber <= 5)
+
+// NOT!
+console.log(!(myFavoriteNumber == 1))
+console.log(!(myFavoriteNumber == 1 || myFavoriteNumber <= 5))
